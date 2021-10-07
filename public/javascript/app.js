4b86d7574c4597ec5289a70f94ea0456883ad94b
Kevin Wilusz <kwilusz10@gmail.com>
2021-10-07 13:29:15 +0200

    done with exercise1

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,1 +1,33 @@
-console.log('I am also alive!')
+console.log('test')
+
+let dataURL = '../json/exercise1.json';
+let res = new XMLHttpRequest();
+res.open('GET', dataURL);
+res.responseType='json';
+res.send();
+
+console.log('data recived');
+
+
+res.onload = () => {
+    const data = res.response;
+    console.log(data);
+
+    const headline = data['headline'];
+    const intro = data['intro'];
+
+    const p1 = document.createElement('p');
+    const h1 = document.createElement('h1');
+
+    
+    p1.textContent = intro;
+    h1.textContent = headline;
+
+    
+
+    document.getElementById('exercise1').appendChild(h1);
+    document.getElementById('exercise1').appendChild(p1);
+
+    
+}
+
